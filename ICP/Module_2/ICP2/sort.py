cd4e60294dcaef950f27a45fe5d8189941cdc8a1
Ruthvicp <ruthvic7@gmail.com>
2018-07-04 23:40:17 -0500

    ICP 2
    matrix mul and merge sort in pyspark

---
+++ ICP/Module_2/ICP2/sort.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import sys
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    """" if len(sys.argv) != 2:
+         print("Usage: sort <file>", file=sys.stderr)
+         sys.exit(-1) """
+
+    spark = SparkSession \
+        .builder \
+        .appName("PythonSort") \
+        .getOrCreate()
+
+
+    lines = spark.read.text("input.txt").rdd.map(lambda r: r[0])
+    sortedCount = lines.flatMap(lambda x: x.split(' ')) \
+        .map(lambda x: (int(x), 1)) \
+        .sortByKey()
+    print(sortedCount)
+    # This is just a demo on how to bring all the sorted data back to a single node.
+    # In reality, we wouldn't want to collect all the data to the driver node.
+    output = sortedCount.collect()
+    print(output)
+    for (num, unitcount) in output:
+        print(num)
+
+    spark.stop()
